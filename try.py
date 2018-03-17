6bd5288b67ae56995a4276e489bd80442b823d99
Troy <q133457@gmail.com>
2018-03-17 20:41:30 +0800

    add try

---
+++ try.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url='https://www.tdcc.com.tw/smWeb/QryStock.jsp?SCA_DATE=20180302&SqlMethod=StockNo&StockNo=1234&StockName=&sub=%ACd%B8%DF'
+
+maxtime=300
+
+while(maxtime):
+	maxtime-=1
+	print(maxtime)
+	try:
+		requests.get(url)
+	except:
+		print('fail')
